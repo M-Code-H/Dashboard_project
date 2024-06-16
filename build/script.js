f6af5340bf9f1c904e0eaa701403bf39b84324b2
M-Code-H <mmkkmm0960@gmail.com>
2024-06-16 14:30:37 +0300

    initial commit

---
+++ build/script.js
@@ -0,0 +1,13 @@
+var icon = document.getElementById("sun");
+var body = document.getElementById("body");
+icon.onclick = function () {
+  body.classList.toggle("dark");
+};
+var icon = document.getElementById("dark");
+var body = document.getElementById("body");
+icon.onclick = function () {
+  body.classList.toggle("dark");
+};
+
+
+console.log(150+75+180)
